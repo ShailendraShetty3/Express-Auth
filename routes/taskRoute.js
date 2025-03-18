19a0d561783650beb7070d14f94bd1ec3156e5d1
unknown <shettyshailendra3@gmail.com>
2025-03-18 08:39:44 +0530

    crud done for task

--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -32,6 +32,38 @@ router.get("/", authMiddleware, async (req, res) => {
     }
   });
 
+
+  router.get("/:id", authMiddleware, async (req, res) => {
+    try {
+      const task = await Task.findById(req.params.id);
+      if (!task) return res.status(404).json({ error: "Task not found" });
+      res.json(task);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  });
+
+
+  router.put("/:id", authMiddleware, async (req, res) => {
+    try {
+      const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+      if (!task) return res.status(404).json({ error: "Task not found" });
+      res.json(task);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  });
+
+
+  router.delete("/:id", authMiddleware, async (req, res) => {
+    try {
+      const task = await Task.findByIdAndDelete(req.params.id);
+      if (!task) return res.status(404).json({ error: "Task not found" });
+      res.json({ message: "Task deleted successfully" });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  });
   
 
   
